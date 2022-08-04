b1e083d6441fcc56f801b4edee929c45c88df1dc
Tobioguntase <tobioguntuase@gmail.com>
2022-08-04 11:53:16 -0500

    update

---
+++ views/science.js
@@ -0,0 +1,115 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+let shuffledQuestions, currentQuestionIndex
+
+function startGame() {
+    console.log('started');
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+}
+
+function setNextQuestion(question) {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+    
+} 
+
+function showQuestion(question) {
+    questionElement.innerText = question.quesiton
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart with random question'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        quesiton: 'What is 33 x 2?',
+        answers: [
+             {text: '66', correct: true },
+             {text: '4', correct: false }
+
+        ]
+    },
+    {
+        quesiton: 'What is 8 + 12?',
+        answers: [
+             {text: '20', correct: true },
+             {text: '67', correct: false }
+
+        ]
+    },
+    {
+        quesiton: 'What is 5 x 2?',
+        answers: [
+             {text: '10', correct: true },
+             {text: '6', correct: false }
+
+        ]
+    },
+    {
+        quesiton: 'What is 44 / 2?',
+        answers: [
+             {text: '22', correct: true },
+             {text: '48', correct: false }
+
+        ]
+    }
+]
