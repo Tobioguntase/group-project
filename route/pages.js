404fea1a54f12df0573020fa497503040b85865a
Stephanie Salgado <83602099+OnlyXSteph@users.noreply.github.com>
2022-08-04 15:43:13 -0500

    DB part 1
    Contact page now works. Everything changed from HTML to EJS. Scripts were moved back to js file under public. Log In and Sign Up files created (forms will be created later). Contact page doesn't allow for use of other navbar items (only home works).

---
+++ route/pages.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const Contact = require('./models/contact');
+
+router.get('/', (req, res) => {
+    res.render('index')
+})
+
+router.get('/mathQuiz', (req, res) => {
+    res.render('mathQuiz')
+})
+
+router.get('/readingWritingQuiz', (req, res) => {
+    res.render('readingWritingQuiz')
+})
+
+router.get('/scienceQuiz', (req, res) => {
+    res.render('scienceQuiz')
+})
+
+router.get('/socialStudiesQuiz', (req, res) => {
+    res.render('socialStudiesQuiz')
+})
+
+router.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+router.post('/submitContact', (req, res) => {
+    const contact = new Contact ({
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        reason: req.body.reason,
+        comment: req.body.comment
+    })
+
+    Contact.collection.insertOne(contact)
+    .then(result => {
+        res.render('contact')
+    })
+
+    .catch(err => console.log(err));
+
+})
+
+module.exports = router;
