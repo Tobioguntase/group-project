938f72abb36f256eda94ae4a920225e58ca9297d
Tobioguntase <tobioguntuase@gmail.com>
2022-08-03 23:56:00 -0500

    update

--- public/js/mathScript.js
+++ public/js/mathScript.js
@@ -1,42 +1,104 @@
 
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById("question-container")
+const quesitonElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn")
 
-const questionContainerElement = document.getElementById('question-container')
-const quesitonElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
 
-let shuffleQuestions,currentQuestionIndex;
-let quizScore =0; 
 
-startButton.addEventListener('click',startGame)
-nextButton.addEventListener('click',() =>{
-    currentQuestionIndex++
+
+
+let shuffledQuestions,currentQuestionIndex
+let quizScore = 0
+
+startButton.addEventListener('click', startGame)
+
+nextButton.addEventListener("click",() =>{
+    currentQuestionIndex++;
+    setnextQuestion();
+});
+
+function startGame() {
+    console.log('started')
+    startButton.classList.add("hide")
+    shuffledQuestions=questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove("hide")
     setnextQuestion()
-})
+    quizScore = 0;
+}
 
-function setStatusClass(element,correct){
-    clearStatusClass(element)
-    if(correct){
-        element.classList.add(correct)
-    }else {
-        element.classList.add(wrong)
+function setNextQuestion() {
+    resetState(); 
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.quesiton;
+    question.answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+        answerButtonsElement.appendChild(button); 
+    });
+}
+
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add("hide");
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     }
 }
 
-function clearStatusClass(element){
-    element.classList.remove(correct)
-    element.classList.remove(wrong)
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach((button) => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide");
+    } else {
+        startButton.innerText = "Restart";
+        startButton.classList.remove("hide");
+    }
+    if (selectedButton.dataset = correct) {
+        quizScore++;
+    }
+    document.getElementById('right-answers').innerHTML = quizScore;
 }
 
-const questions =[
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct");
+    element.classList.remove("wrong"); 
+}
+
+var questions = [
     {
-        quesiton: 'What is 33 x 2?',
-        answers :[
-            { Text: '332', correct: false},
-            { Text: '35', correct: false},
-            { Text: '66', correct: true},
-            { Text: '31', correct: false},
-        ]
+        "quesiton" : "What is 33 x 2?",
+        "answers" : [
+             "35",
+             "4",
+             "9",
+             "66", 
+        ],
+        "answerId" : 66
+
     }
 ]
