51e2d7a754cce236754b9f3c182190dbd5cdf096
Tobioguntase <tobioguntuase@gmail.com>
2022-08-04 00:10:22 -0500

    Update mathScript.js

--- public/js/mathScript.js
+++ public/js/mathScript.js
@@ -1,104 +1,15 @@
-
-const startButton = document.getElementById("start-btn")
-const questionContainerElement = document.getElementById("question-container")
-const quesitonElement = document.getElementById("question")
-const answerButtonsElement = document.getElementById("answer-buttons")
-const nextButton = document.getElementById("next-btn")
-
-
-
-
-
-let shuffledQuestions,currentQuestionIndex
-let quizScore = 0
+const startButton = document.getElementById('start-btn')
 
 startButton.addEventListener('click', startGame)
 
-nextButton.addEventListener("click",() =>{
-    currentQuestionIndex++;
-    setnextQuestion();
-});
-
 function startGame() {
     console.log('started')
-    startButton.classList.add("hide")
-    shuffledQuestions=questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0;
-    questionContainerElement.classList.remove("hide")
-    setnextQuestion()
-    quizScore = 0;
 }
 
 function setNextQuestion() {
-    resetState(); 
-    showQuestion(shuffledQuestions[currentQuestionIndex]);
+
+} 
+
+function selectAnswer() {
+
 }
-
-function showQuestion(question) {
-    questionElement.innerText = question.quesiton;
-    question.answers.forEach((answer) => {
-        const button = document.createElement("button");
-        button.innerText = answer.text;
-        button.classList.add("btn");
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-        answerButtonsElement.appendChild(button); 
-    });
-}
-
-function resetState() {
-    clearStatusClass(document.body);
-    nextButton.classList.add("hide");
-    while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
-    }
-}
-
-function selectAnswer(e) {
-    const selectedButton = e.target;
-    const correct = selectedButton.dataset.correct;
-    setStatusClass(document.body, correct);
-    Array.from(answerButtonsElement.children).forEach((button) => {
-        setStatusClass(button, button.dataset.correct);
-    });
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
-        nextButton.classList.remove("hide");
-    } else {
-        startButton.innerText = "Restart";
-        startButton.classList.remove("hide");
-    }
-    if (selectedButton.dataset = correct) {
-        quizScore++;
-    }
-    document.getElementById('right-answers').innerHTML = quizScore;
-}
-
-function setStatusClass(element, correct) {
-    clearStatusClass(element);
-    if (correct) {
-        element.classList.add("correct");
-    } else {
-        element.classList.add("wrong");
-    }
-}
-
-function clearStatusClass(element) {
-    element.classList.remove("correct");
-    element.classList.remove("wrong"); 
-}
-
-var questions = [
-    {
-        "quesiton" : "What is 33 x 2?",
-        "answers" : [
-             "35",
-             "4",
-             "9",
-             "66", 
-        ],
-        "answerId" : 66
-
-    }
-]
